d2bd5885e1922d75600ec7a549bbe2356d439f11
Hana <grace.sambora@gmail.com>
2024-07-22 01:05:13 -0500

    Add user and game info to review fetch request

--- client/src/pages/AllReviews.js
+++ client/src/pages/AllReviews.js
@@ -8,9 +8,20 @@ function AllReviews(){
         fetch("/reviews")
         .then((r) => r.json())
         .then((reviewData) => {
-            console.log("Before set: ", reviews)
-            console.log(typeof(reviewData))
-            setReviews(Object.values(reviewData))
+            userInfo = Object.values(reviewData).map((review)=>{
+                fetch("/users/${review.user_id")
+                .then((r) => r.json())
+            })
+            gameInfo = Object.values(reviewData).map((review)=>{
+                fetch("/games/${review.game_id")
+                .then((r)=> r.json())
+            })
+
+            const updatedReviews = Object.values(reviewData).map((review, index)=>(
+                {...review, user: userInfo[index], game:gameInfo[index]}
+            ))
+            
+            setReviews(updatedReviews)
         })
     }, []);
 
@@ -21,7 +32,7 @@ function AllReviews(){
         {reviews.length === 0 ? (<p>No reviews available.</p>) :(
              reviews.map((review) => {
                 console.log(review)
-            return <Review username={review.username} pfp={review.pfp} title={review.title} recommend={review.recommend} text={review.rev_text}/>})
+                return <Review username={review.user.username} pfp={review.user.pfp_image_url} title={review.title} recommend={review.recommend} text={review.rev_text}/>})
         )}
         </>
     )
