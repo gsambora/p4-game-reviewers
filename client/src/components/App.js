48aee21beae47a0ca635aa36fc41d4f71dceb057
Hana <grace.sambora@gmail.com>
2024-07-21 23:35:35 -0500

    Displayed review information in /allreviews route

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AllReviews from "../pages/AllReviews";
 
 function App() {
-  return <h1>Project Client</h1>;
+  return (
+    <BrowserRouter>
+      <h1>Project Client</h1>
+      <main>
+        <Switch>
+          <Route path="/allreviews">
+            <AllReviews />
+          </Route>
+        </Switch>
+      </main>
+    </BrowserRouter>
+    
+  );
+
 }
 
 export default App;
---
+++ client/src/pages/AllReviews.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+function AllReviews(){
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(()=>{
+        fetch("/reviews")
+        .then((r) => r.json())
+        .then((reviewData) => {
+            console.log("Before set: ", reviews)
+            console.log(typeof(reviewData))
+            setReviews(Object.values(reviewData))
+        })
+    }, []);
+
+    console.log("After set: ", reviews, typeof(reviews))
+    return(
+        <>
+        <h1>All Reviews:</h1>
+        {reviews.length === 0 ? (<p>No reviews available.</p>) :(
+             reviews.map((review) => {
+            return <p key={review.id}>{review.rev_text}</p>})
+        )}
+        </>
+    )
+}
+
+export default AllReviews;
